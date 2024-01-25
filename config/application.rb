ee13deadf53bd589b325dfc5c15455883854b066
Kazım Aydin <kazim.aydin@dia.com.tr>
2024-01-25 11:37:20 +0300

    superform:install

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module SuperformRails
   class Application < Rails::Application
+    config.autoload_paths << "#{root}/app/views/forms"
     config.autoload_paths << "#{root}/app/views"
     config.autoload_paths << "#{root}/app/views/layouts"
     config.autoload_paths << "#{root}/app/views/components"
